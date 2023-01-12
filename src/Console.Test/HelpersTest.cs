fb8c8534d6b72d5cce1712f83ba5619866017836
Leonardo Coelho <leonardocoel98@gmail.com>
2023-01-12 14:47:46 -0300

    test: implement tests for Helpers class

--- src/Console.Test/HelpersTest.cs
+++ src/Console.Test/HelpersTest.cs
@@ -1,12 +1,31 @@
 using Xunit;
+using FluentAssertions;
+using ConsoleProgram;
 
 namespace Console.Test;
 
 public class HelpersTest
 {
-    [Theory]
-    public void Test1()
-    {
+    [Theory(DisplayName = "Should get the all characters up to the selected one")]
+    [InlineData('A', "A")]
+    [InlineData('M', "ABCDEFGHIJKLM")]
+    [InlineData('Z', "ABCDEFGHIJKLMNOPQRSTUVWXYZ")]
 
+    public void TestGetCharacters(char character, string expected)
+    {
+        var result = Helpers.GetCharacters(character);
+
+        result.Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "Should invert all characters in the string")]
+    [InlineData("ABC", "CBA")]
+    [InlineData("ABCDEFGHIJKLM", "MLKJIHGFEDCBA")]
+    [InlineData("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ZYXWVUTSRQPONMLKJIHGFEDCBA")]
+    public void TestInvertString(string characters, string expected)
+    {
+        var result = Helpers.InvertString(characters);
+
+        result.Should().Be(expected);
     }
 }
