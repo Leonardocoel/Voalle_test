9757337482666d21dced414171752ee3ed1fdaa3
Leonardo Coelho <leonardocoel98@gmail.com>
2023-01-13 16:40:39 -0300

    test: implement test for Square method in Polygons class

--- src/Console.Test/PolygonsTest.cs
+++ src/Console.Test/PolygonsTest.cs
@@ -9,8 +9,6 @@ public class PolygonsTest
     [Theory(DisplayName = "Should return a diamond shaped polygon using the correct characters")]
     [InlineData('C', "   A\n B   B\nC     C\n B   B\n   A")]
     [InlineData('E', "     A\n   B   B\n  C     C\n D       D\nE         E\n D       D\n  C     C\n   B   B\n     A")]
-
-
     public void TestDiamond(char character, string expected)
     {
         var result = Polygons.Diamond(character);
@@ -18,6 +16,13 @@ public class PolygonsTest
         result.Should().Be(expected);
     }
 
+    [Theory]
+    [InlineData('C', " A A A A A\n B       B\n C       C\n B       B\n A A A A A")]
+    [InlineData('E', " A A A A A A A A A\n B               B\n C               C\n D               D\n E               E\n D               D\n C               C\n B               B\n A A A A A A A A A")]
+    public void TestSquare(char character, string expected)
+    {
+        var result = Polygons.Square(character);
 
+        result.Should().Be(expected);
+    }
 }
-
