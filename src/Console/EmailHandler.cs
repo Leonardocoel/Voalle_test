e631013ec5883f1f43784d0f2f10254d3be608bb
Leonardo Coelho <leonardocoel98@gmail.com>
2023-01-14 11:28:29 -0300

    refactor: simplifies changing credentials

--- src/Console/EmailHandler.cs
+++ src/Console/EmailHandler.cs
@@ -10,11 +10,15 @@ namespace ConsoleProgram;
 public static class EmailHandler
 {
 
+    private static readonly string _SenderName = "Leonardo Coelho";//! Alterar 
+    private static readonly string _SenderEmail = "Email";//! Alterar 
+    private static readonly string _SenderPassword = "Password";//! Alterar 
+
     public static void Send(string email, string text)
     {
         var message = new MimeMessage();
 
-        message.From.Add(new MailboxAddress("Leonardo Coelho", "leonardocoel98@gmail.com"));//! Alterar 
+        message.From.Add(new MailboxAddress(_SenderName, _SenderEmail));
         message.To.Add(MailboxAddress.Parse(email));
         message.Subject = "Here is the result of your request";
         message.Body = new TextPart("plain")
@@ -28,8 +32,8 @@ public static class EmailHandler
         {
             // MICROSOFT => https://support.microsoft.com/pt-br/office/configura%C3%A7%C3%B5es-pop-imap-e-smtp-8361e398-8af4-4e97-b147-6c6c4ac95353
             // GMAIL => https://support.google.com/mail/answer/7126229?hl=pt-BR#zippy=%2Cetapa-alterar-o-smtp-e-outras-configura%C3%A7%C3%B5es-no-seu-cliente-de-e-mail%2Cn%C3%A3o-consigo-fazer-login-no-meu-cliente-de-e-mail
-            client.Connect("smtp.office365.com", 587, false);
-            client.Authenticate("email", "senha");//! INSERIR EMAIL E SENHA AQUI
+            client.Connect("smtp.office365.com", 587, false);//! Alterar de acordo com o provedor o e-mail
+            client.Authenticate(_SenderEmail, _SenderPassword);
 
             client.Send(message);
 
