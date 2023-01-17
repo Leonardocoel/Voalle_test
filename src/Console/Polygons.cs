4bd314d8cf19ff2a2b16e8c2b3b74899c5a01973
Leonardo Coelho <leonardocoel98@gmail.com>
2023-01-17 13:03:46 -0300

    refactor: Improves diamond creation logic

--- src/Console/Polygons.cs
+++ src/Console/Polygons.cs
@@ -6,41 +6,33 @@ namespace ConsoleProgram;
 public static class Polygons
 {
 
-    public static string Diamond(char character)
+    public static string Diamond(char letter)
     {
         var strBuilder = new StringBuilder("");
 
-        var alpha = Helpers.GetCharacters(character);
-        var alphaInverted = Helpers.InvertString(alpha)[1..^1];
+        var alpha = Helpers.GetCharacters(letter);
+        var alphaSelected = alpha[1..].Select((c, i) => (c, i));
 
-        int lenght = alpha.Length;
+        var LineA = new string(' ', alpha.Length) + "A" + "\n";
+        strBuilder.Insert(0, LineA, 2);
 
-        strBuilder.Append("A".PadLeft(lenght + 1) + "\n");
-        strBuilder.Append("B".PadLeft(lenght - 1) + "   B\n");//min 1 space in B
-
-
-        var alphaSliced = alpha[2..];
-        foreach (var (c, i) in alphaSliced.Select((c, i) => (c.ToString(), i)))
+        foreach (var (character, i) in alphaSelected)
         {
-            var left = alphaSliced.Length - i;
-            var initalSpacing = 6; // min 4
-            var middle = initalSpacing + (2 * i);
+            int leftSpacingCount = alpha[1..^1].Length - i;
+            string leftSpacing = new(' ', leftSpacingCount);
+
+            int initialSpacing = 3; //B
+            int middleSpacingCount = initialSpacing + 2 * i;
+            string middleSpacing = new(' ', middleSpacingCount);
 
 
-            strBuilder.Append(c.PadLeft(left) + c.PadLeft(middle) + "\n");
+            var insertIndex = strBuilder.Length / 2;
+            var diamondLine = leftSpacing + character + middleSpacing + character + '\n';
+            int repeatN = (character == letter) ? 1 : 2;
+
+
+            strBuilder.Insert(insertIndex, diamondLine, repeatN);
         }
-
-        foreach (var (c, i) in alphaInverted.Select((c, i) => (c.ToString(), i)))
-        {
-            var left = 2 + i;
-            var initalSpacing = 2;//min 0
-            var middle = (alphaInverted.Length - i) * 2 + initalSpacing;
-
-            strBuilder.Append(c.PadLeft(left) + c.PadLeft(middle) + "\n");
-        }
-
-        strBuilder.Append("A".PadLeft(lenght + 1));
-
 
         return strBuilder.ToString();
     }
@@ -70,3 +62,32 @@ public static class Polygons
         return strBuilder.ToString();
     }
 }
+
+// var alphaInverted = Helpers.InvertString(alpha)[1..^1];
+// int lenght = alpha.Length;
+
+//         strBuilder.Append("A".PadLeft(lenght + 1) + "\n");
+//         strBuilder.Append("B".PadLeft(lenght - 1) + "   B\n");//min 1 space in B
+
+
+//         var alphaSliced = alpha[2..];
+//         foreach (var (c, i) in alphaSliced.Select((c, i) => (c.ToString(), i)))
+//         {
+//             var left = alphaSliced.Length - i;
+//             var initalSpacing = 6; // min 4
+//             var middle = initalSpacing + (2 * i);
+
+
+//             strBuilder.Append(c.PadLeft(left) + c.PadLeft(middle) + "\n");
+//         }
+
+//         foreach (var (c, i) in alphaInverted.Select((c, i) => (c.ToString(), i)))
+//         {
+//             var left = 2 + i;
+//             var initalSpacing = 2;//min 0
+//             var middle = (alphaInverted.Length - i) * 2 + initalSpacing;
+
+//             strBuilder.Append(c.PadLeft(left) + c.PadLeft(middle) + "\n");
+//         }
+
+//         strBuilder.Append("A".PadLeft(lenght + 1));
