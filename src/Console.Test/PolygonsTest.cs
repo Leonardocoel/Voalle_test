f24344c9896938eefbcf6d008237dd22220d523a
Leonardo Coelho <leonardocoel98@gmail.com>
2023-01-17 13:06:31 -0300

    test: add a space at the end of inlinedata of TestDiamond

--- src/Console.Test/PolygonsTest.cs
+++ src/Console.Test/PolygonsTest.cs
@@ -7,8 +7,8 @@ namespace ConsoleProgram.Test;
 public class PolygonsTest
 {
     [Theory(DisplayName = "Should return a diamond shaped polygon using the correct characters")]
-    [InlineData('C', "   A\n B   B\nC     C\n B   B\n   A")]
-    [InlineData('E', "     A\n   B   B\n  C     C\n D       D\nE         E\n D       D\n  C     C\n   B   B\n     A")]
+    [InlineData('C', "   A\n B   B\nC     C\n B   B\n   A\n")]
+    [InlineData('E', "     A\n   B   B\n  C     C\n D       D\nE         E\n D       D\n  C     C\n   B   B\n     A\n")]
     public void TestDiamond(char character, string expected)
     {
         var result = Polygons.Diamond(character);
